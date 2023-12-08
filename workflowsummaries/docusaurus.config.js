19e36262c168f38db58ecf2c6361261c252e63c4
DanRoscigno <dan@roscigno.com>
2023-12-08 12:30:32 -0500

    report build output
    Signed-off-by: DanRoscigno <dan@roscigno.com>

--- workflowsummaries/docusaurus.config.js
+++ workflowsummaries/docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
   projectName: 'docusaurus', // Usually your repo name.
   trailingSlash: true,
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
