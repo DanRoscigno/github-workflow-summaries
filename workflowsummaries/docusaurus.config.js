a6f3df6354026b99e6e7e592c5b6101ffcea54ec
DanRoscigno <dan@roscigno.com>
2023-12-08 11:28:43 -0500

    fix links to dir
    Signed-off-by: DanRoscigno <dan@roscigno.com>

--- workflowsummaries/docusaurus.config.js
+++ workflowsummaries/docusaurus.config.js
@@ -78,7 +78,7 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          {to: '/blog/', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
@@ -120,7 +120,7 @@ const config = {
             items: [
               {
                 label: 'Blog',
-                to: '/blog',
+                to: '/blog/',
               },
               {
                 label: 'GitHub',
